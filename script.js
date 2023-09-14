f1fddeba0015f10a7348b23d3cc3288cf85110a1
MohammadEl-khatib <shaveddoor@gmail.com>
2023-09-14 18:40:23 -0400

    repo

---
+++ script.js
@@ -0,0 +1,25 @@
+function init(){
+    const name = prompt("Welcome to GC mini golf! What is your name?");
+    const numHoles = Number( prompt(`Hi ${name}! Would you like to play 3 or 6 holes today?`));
+    const par = numHoles === 6 ? 18 : 9;
+    let putts = 0;
+
+    for (let i = 0; i < numHoles; i++) {
+        putts += Number(prompt(`How many putts for hole ${i+1}? (par is ${par})`));
+
+        // The last hole
+        if (i + 1 === numHoles) {
+            if (putts > par) {
+                console.log(`Nice try, ${name}... Your total par was: +${par}`);
+            } else if (putts === par) {
+                console.log(`Good game, ${name}! Your total par was 0`);
+            } else {
+                console.log(`Great job, ${name}! Your total par was -${par}`);
+            }
+        }
+    }
+
+    console.log(`Good game, ${name}! Your total par was: ${putts-par}`);
+}
+
+init();
