b4b0a5939cba50681558a08649dbb71ef5486cd8
MohammadEl-khatib <shaveddoor@gmail.com>
2023-09-15 18:21:41 -0400

    fixed console logs

--- script.js
+++ script.js
@@ -4,22 +4,23 @@ function init(){
     const par = numHoles === 6 ? 18 : 9;
     let putts = 0;
 
+    console.log({par, numHoles})
     for (let i = 0; i < numHoles; i++) {
         putts += Number(prompt(`How many putts for hole ${i+1}? (par is ${par})`));
-
+        console.log({putts})
         // The last hole
         if (i + 1 === numHoles) {
             if (putts > par) {
-                console.log(`Nice try, ${name}... Your total par was: +${par}`);
+                console.log(`Nice try, ${name}... Your total par was: +${putts - par}`);
             } else if (putts === par) {
                 console.log(`Good game, ${name}! Your total par was 0`);
             } else {
-                console.log(`Great job, ${name}! Your total par was -${par}`);
+                console.log(`Great job, ${name}! Your total par was ${putts - par}`);
             }
         }
     }
 
-    console.log(`Good game, ${name}! Your total par was: ${putts-par}`);
+    // console.log(`Good game, ${name}! Your total par was: ${putts-par}`);
 }
 
 init();
